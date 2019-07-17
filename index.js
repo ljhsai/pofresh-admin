e0c86dede64002da333f898b8e303ffc90a0a3d0
lujinhua <ljhxai@163.com>
2019-07-17 19:03:45 +0800

    添加protocols

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ module.exports.createMasterConsole = consoleService.createMasterConsole;
 module.exports.createMonitorConsole = consoleService.createMonitorConsole;
 module.exports.adminClient = require('./lib/client/client');
 
+exports.protocols = {
+	MQTTServer: require('./lib/protocol/mqtt/mqttServer'),
+	MQTTClient: require('./lib/protocol/mqtt/mqttClient'),
+
+	SIOServer: require('./lib/protocol/socketio/sioServer'),
+	SIOClient: require('./lib/protocol/socketio/sioClient'),
+}
+
 exports.modules = {};
 fs.readdirSync(__dirname + '/lib/modules').forEach(function(filename) {
 	if (/\.js$/.test(filename)) {
